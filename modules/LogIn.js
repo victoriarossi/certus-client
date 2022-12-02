80c35f7aaf05aa684a2ac3ef0e260bd9f4a7aa5e
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-12-01 22:48:16 -0300

    Working Login

--- modules/LogIn.js
+++ modules/LogIn.js
@@ -5,6 +5,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Paper from '@mui/material/Paper';
 import {Grid, TextField} from "@mui/material";
+import Button from "@mui/material/Button";
 
 function GridItem(theme){
     return(
@@ -19,8 +20,13 @@ function LogIn(){
         <>
             <Head><title>Certus - Log In</title></Head>
             <AppAppBar class={AppAppBar}></AppAppBar>
-            <Box
+            <Grid
                 position="fixed"
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
                 sx={{
                     marginTop: 0,
                     width: '100%',
@@ -29,41 +35,50 @@ function LogIn(){
                 }}
             >
 
+                <div className="description-login font colorfont1 center-text" style={{color: '#D9FCF3', marginBottom: 50}}>
+                    LOG IN
+                </div>
                 <Box
                     sx={{
-                        marginTop: '100',
                         marginLeft: 'auto',
                         marginRight: 'auto',
                         width: 500,
-                        height: 600,
+                        padding: 5,
                         borderRadius: 10,
                         backgroundColor: '#D9FCF3',
                     }}
                 >
+
                     <div
                         style={{
+                            height: '100%',
                             display: 'flex',
                             justifyContent: 'center',
-                            alignItems: 'center',
-                            height: '100%'
+                            alignItems: 'center'
                         }}
                     >
+
                         <Grid
                             contairner
                         >
                             <Grid item xs={12}>
-                                Log In
+                                <TextField fullWidth label="user" id="fullWidth" style={{width: '100%', marginTop: 10, marginBottom: 10}} className="textFieldColor"/>
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField id="standard-basic" label="user" variant="standard"/>
+                                <TextField fullWidth label="password" id="fullWidth" style={{width: '100%', marginTop: 10, marginBottom: 10}} type="password"/>
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField id="standard-basic" label="user" variant="standard"/>
+                                <Button variant="outlined" style={{marginTop: 10, marginBottom: 10, justifySelf: "center"}}>Log In</Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <div className="text-login font colorfont1 center-text" style={{color: '#041D3D'}}>
+                                    Not have an account? Sign Up now
+                                </div>
                             </Grid>
                         </Grid>
                     </div>
                 </Box>
-            </Box>
+            </Grid>
         </>
     )
 }
