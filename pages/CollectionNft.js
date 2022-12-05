35a89c1252737cf2a4591f18f1024eb23c6da388
lautarohernando <lautihernando@gmail.com>
2022-12-05 01:47:24 -0300

    Transfer page

--- pages/CollectionNft.js
+++ pages/CollectionNft.js
@@ -7,13 +7,22 @@ import Box from "@mui/material/Box";
 import {Grid, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import Web3 from "web3";
-
-
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import { BarLoader,DoubleBubble, SlidingPebbles,DoubleOrbit, Spinner} from 'react-spinner-animated';
+import 'react-spinner-animated/dist/index.css'
 export default function CollectionNft(){
     //let provider = window.ethereum;
     let selectedAccount;
 
-    const [account, setAccount,to] = useState("");
+    const [account, setAccount] = useState("");
+    const [toAddress,setToAddress] = useState("")
+    const [tokenId,setTokenId] = useState("")
+    const [open, setOpen] = useState(false);
+    const closeModal = () => setOpen(false);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8080");
   async function loadBlockChain() {
     ethereum
@@ -28,7 +37,12 @@ export default function CollectionNft(){
   }
 
   useEffect(() => loadBlockChain, []);
-
+ function onChange(e){
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+       setTokenId( e.target.value)
+    }
+ }
 
                                                             
 
@@ -61,18 +75,28 @@ async function loadContract(){
     
 
       var myContractInstance = new web3.eth.Contract(chainlinkFutureContractABI, address);
-      myContractInstance.methods.transferFrom(account,"0xD9429F1567355B5532B0F2051b2e565eA4cB302b",185)
+      try{
+      setError(false)
+      setLoading(true)
+      myContractInstance.methods.transferFrom(account,toAddress,parseInt(tokenId,10))
          .send({
                  from: account, 
                  gas: 0x00, 
                  gasPrice: 0x00
-        }).then(receipt=> {console.log(receipt)});
-        }
+        }).then(receipt=> {
+            setOpen(o => !o); setLoading(false)} ).catch(error => {setError(o => !o);setLoading(false)});
+    }catch(e){
+        setLoading(false)
+        setError(o => !o)
+    }
+    }
 
-
+        
  
     return (
-        <>
+        <> 
+          
+          
             <Head><title>Certus - Collections</title></Head>
             <DashboardAppBar></DashboardAppBar>
             <Grid
@@ -89,7 +113,16 @@ async function loadContract(){
                     backgroundColor: '#D9FCF3',
                 }}
             >
+          
+            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+                <div className="modal">
+                 <a className="close" onClick={closeModal}>
+                    &times;
+                    Your transfer de NFT
+                </a>
 
+                </div>
+            </Popup>
                 <div className="description-login font colorfont1 center-text" style={{color: '#11e3ab', marginBottom: 50}}>
                     Transfer NFT
                 </div>
@@ -103,7 +136,7 @@ async function loadContract(){
                         backgroundColor: '#11e3ab',
                     }}
                 >
-
+            
                     <div
                         style={{
                             height: '100%',
@@ -112,22 +145,34 @@ async function loadContract(){
                             alignItems: 'center'
                         }}
                     >
-
+                    {!loading?
                         <Grid
                             contairner
                         >
+                            {error?
+                            <p color="red">Error tranfering</p>
+                            : null
+                        }
+                       
                             <Grid item xs={12}>
-                                <TextField fullWidth label="Wallet" id="fullWidth" style={{width: '100%', marginTop: 10, marginBottom: 10}} className="textFieldColor" />
+                                <TextField fullWidth label="Wallet" id="fullWidth" style={{width: '100%', marginTop: 10, marginBottom: 10}} onChange={(e)=> setToAddress(e.target.value)} className="textFieldColor" />
+                                <TextField fullWidth value={tokenId} label="Token Id" id="fullWidth" style={{width: '100%', marginTop: 10, marginBottom: 10}} onChange={(e)=>onChange(e)} className="textFieldColor" />
+
                             </Grid>
 
                             <Grid item xs={12}>
                                 <Button style={{marginTop: 10, marginBottom: 10, backgroundColor:'#D9FCF3', color: '#11e3ab'}} variant="contained" onClick={loadContract}>Transfer</Button>
-                                <p>Your account: {account}</p>
                             </Grid>
-
+                            
+                                                  
                         </Grid>
+                         :
+                         <Spinner text={"Loading..."} bgColor={"#D9FCF3"} 
+                         center={false} width={"120px"} height={"120px"}/>
+                         }
                     </div>
                 </Box>
+                
             </Grid>
         </>
     )
