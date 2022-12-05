6e42037f0e844d89ddebeba3a2282b379d475ffc
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-12-05 14:54:45 -0300

    Destination wallet textfield

---
+++ pages/MintedOk.js
@@ -0,0 +1,72 @@
+import Head from "next/head";
+import AppAppBar from "../modules/AppAppBar";
+import {Grid, TextField} from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import * as React from "react";
+
+
+export default function MintedOk(){
+
+
+
+    return (
+        <>
+            <Head><title>Certus - Contact Us</title></Head>
+            <AppAppBar class={AppAppBar}></AppAppBar>
+            <Grid
+                position="fixed"
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                sx={{
+                    marginTop: 0,
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: '#D9FCF3',
+                }}
+            >
+
+                <Box
+                    sx={{
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        width: 500,
+                        padding: 5,
+                        borderRadius: 10,
+                        backgroundColor: '#11e3ab',
+                    }}
+                >
+
+                    <div
+                        style={{
+                            height: '100%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                    >
+
+                        <Grid
+                            container
+                        >
+                            <Grid item xs={12}>
+                                <TextField fullWidth label="Business Name" id="fullWidth" style={{width: '100%', marginTop: 10, marginBottom: 10}} className="textFieldColor"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField fullWidth label="Business Field" id="fullWidth" style={{width: '100%', marginTop: 10, marginBottom: 10}} className="textFieldColor"/>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <Button style={{marginTop: 10, marginBottom: 10, backgroundColor:'#11e3ab'}} variant="contained" >Contact Us</Button>
+                            </Grid>
+
+                        </Grid>
+                    </div>
+                </Box>
+            </Grid>
+        </>
+    )
+}
